5cc33515d65142ac367dc3c8068354bc08ff05a4
Maxwell Leonard <maxwellleon55243@gmail.com>
2023-12-26 16:22:20 -0500

    use sphinx rtd theme

--- conf.py
+++ conf.py
@@ -14,7 +14,9 @@ author = 'b0vik'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
